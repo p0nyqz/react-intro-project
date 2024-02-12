479f8e4930dbc9e3925a709b2e911dd2dc2c7e51
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2024-02-12 01:34:08 +0100

    feat: Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from './components/Header'
 import WayToTeach from './components/WayToTeach'
-import Button from './components/Button'
+import Button from './components/Button/Button'
 import { ways } from './data'
 
 export default function App() {
+  function handleClick(type) {
+    console.log('button clicked', type)
+  }
+
   return (
     <>
       <Header />
@@ -23,6 +27,9 @@ export default function App() {
         </section>
         <section>
           <h3>Чем мы отличаемся от других</h3>
+          <Button onClick={() => handleClick('way')}>Подход</Button>
+          <Button onClick={() => handleClick('easy')}>Доступность</Button>
+          <Button onClick={() => handleClick('program')}>Концентрация</Button>
         </section>
       </main>
     </>
