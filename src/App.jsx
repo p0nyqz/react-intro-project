8051b632f122fdedf1aa0e40d1f14aaebd98b9e3
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2024-02-12 23:54:51 +0100

    feat: WayToTeach map, clean up

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,8 @@ export default function App() {
   const [contentType, setContentType] = useState()
 
   function handleClick(type) {
-    // console.log('button clicked', type)
     setContentType(type)
   }
-
-  // let tabContent = null
-  // if (contentType) {
-  //   tabContent = <p>{differences[contentType]}</p>
-  // } else {
-  //   tabContent = <p>Press the button</p>
-  // }
 
   return (
     <>
@@ -27,13 +19,9 @@ export default function App() {
         <section>
           <h3>Наш подход к обучению</h3>
           <ul>
-            <WayToTeach
-              title={ways[0].title}
-              description={ways[0].description}
-            />
-            <WayToTeach {...ways[1]} />
-            <WayToTeach {...ways[2]} />
-            <WayToTeach {...ways[3]} />
+            {ways.map((way) => (
+              <WayToTeach key={way.title} {...way} />
+            ))}
           </ul>
         </section>
         <section>
@@ -56,18 +44,10 @@ export default function App() {
           >
             Концентрация
           </Button>
-          {/* {contentType ? (
-            <p>{differences[contentType]}</p>
-          ) : (
-            <p>Press the button</p>
-          )} */}
-          {/* {!contentType ? <p>Press the button</p> : null}
-          {contentType ? <p>{differences[contentType]}</p> : null} */}
 
           {!contentType && <p>Press the button</p>}
           {contentType && <p>{differences[contentType]}</p>}
 
-          {/* {tabContent} */}
         </section>
       </main>
     </>
