eed576e37565603d762c17eafe17f17eea71102d
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2024-03-08 04:09:30 +0100

    feat: intro section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import DifferencesSection from './components/DifferencesSection'
 import Header from './components/Header'
+import IntroSection from './components/IntroSection'
 import TeachingSection from './components/TeachingSection'
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
     <>
       <Header />
       <main>
+        <IntroSection />
         <TeachingSection />
         <DifferencesSection />
       </main>
