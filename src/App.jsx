6f3ed611e97ace7afc10e7fcf1cea8c0e5466be0
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2024-03-11 04:48:29 +0100

    feat: routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 import DifferencesSection from './components/DifferencesSection'
+import FeedbackSection from './components/FeedbackSection'
 import Header from './components/Header'
 import IntroSection from './components/IntroSection'
+import TabsSection from './components/TabsSection'
 import TeachingSection from './components/TeachingSection'
+import { useState } from 'react'
 
 export default function App() {
+  const [tab, setTab] = useState('feedback')
+
   return (
     <>
       <Header />
       <main>
         <IntroSection />
-        <TeachingSection />
-        <DifferencesSection />
+        <TabsSection active={tab} onChange={(current) => setTab(current)} />
+
+        {tab === 'main' && (
+          <>
+            <TeachingSection />
+            <DifferencesSection />
+          </>
+        )}
+
+        {tab === 'feedback' && <FeedbackSection />}
       </main>
     </>
   )
