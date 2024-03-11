603311fe129163b04fb93f0145a17cdba900ccc2
p0nyqz <93746632+p0nyqz@users.noreply.github.com>
2024-03-11 04:24:39 +0100

    feat: same code without JSX

--- src/components/IntroSection.jsx
+++ src/components/IntroSection.jsx
@@ -1,11 +1,26 @@
+// export default function IntroSection() {
+//   return (
+//     <section>
+//       <h1 className="centered">Result University</h1>
+//       <h3 className="centered" style={{ color: '#666' }}>
+//         Университет frontend-разработки, который насыщает ІТ-сферу
+//         квалифицированными программистами
+//       </h3>
+//     </section>
+//   )
+// }
+
+import React from 'react'
+
+const e = React.createElement
+
 export default function IntroSection() {
-  return (
-    <section>
-      <h1 className="centered">Result University</h1>
-      <h3 className="centered" style={{ color: '#666' }}>
-        Университет frontend-разработки, который насыщает ІТ-сферу
-        квалифицированными программистами
-      </h3>
-    </section>
-  )
+  return e('section', null, [
+    e('h1', { className: 'centered', key: 1 }, 'Result University'),
+    e(
+      'h3',
+      { className: 'centered', style: { color: '#666' }, key: 2 },
+      'Университет frontend-разработки, который насыщает IT-сферу квалифицированными программистами'
+    ),
+  ])
 }
